5ef06f80059869270b04c086b69c69a93a53e86a
hynek <hynek.bucek@gmail.com>
2023-06-15 09:00:40 +0200

    algorithms added - binary search

---
+++ example/include/binary_search.h
@@ -0,0 +1,37 @@
+/*
+- logarithmic complexity
+- could be used on sorted data
+- very fast
+- https://www.geeksforgeeks.org/binary-search/
+- start in middle compare if value is less or greater then middle / root if less continue on left if greater continue on right half, continue with same alg.
+
+binarySearch(arr, x, low, high)
+	repeat till low = high
+		mid = (low + high)/2
+			if (x = arr[mid])
+				return mid
+			else if (x > arr[mid])
+				low = mid + 1
+			else
+				high = mid - 1
+*/
+
+#pragma once
+
+int BinarySearch(int arr[], int l, int r, int x)
+{
+	while (l <= r) {
+		int m = l + (r - l) / 2;
+		// Check if x is present at mid
+		if (arr[m] == x)
+			return m;
+		// If x greater, ignore left half
+		if (arr[m] < x)
+			l = m + 1;
+		// If x is smaller, ignore right half
+		else
+			r = m - 1;
+	}
+	// if we reach here, then element was not present
+	return -1;
+}
---
+++ example_test/binary_search_UT.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "include\binary_search.h"
+
+using namespace std;
+
+namespace algorithms
+{
+
+TEST(AlgorightmsTest, ShouldFindElementWithBinarySearch)
+{
+	// GIVEN
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int x = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	// WHEN
+	int result = BinarySearch(arr, 0, n - 1, x);
+
+	// THEN
+	EXPECT_EQ(result, 3);
+}
+
+}
