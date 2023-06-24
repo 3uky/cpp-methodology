1fdfe25b20d0c9b2a744d844a05cd82f648af561
hynek <hynek.bucek@gmail.com>
2023-06-24 19:08:47 +0200

    bubble sort implementation + revision modification + unit test

---
+++ example/include/BubbleSort.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <algorithm>
+
+namespace algorithms
+{
+
+class BubbleSort
+{
+public:
+    static void Sort(std::vector<int>& arr)
+    {
+        int n = static_cast<int>(arr.size());
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    std::swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+    }
+
+    static void RevisedSort(std::vector<int>& arr)
+    {
+        int n = static_cast<int>(arr.size());
+
+        for (int i = 0; i < n; i++)
+        {
+            bool isSwapped = false;
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    std::swap(arr[j], arr[j + 1]);
+                    isSwapped = true;
+                }
+            }
+            if (!isSwapped)
+                break;
+        }
+    }
+};
+
+}
