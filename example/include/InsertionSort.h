e6c99c098b7ec8a8fd8b4495513042e2ea0ecae0
hynek <hynek.bucek@gmail.com>
2023-06-24 21:17:08 +0200

    insertion sort + ut

---
+++ example/include/InsertionSort.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <algorithm>
+
+// https://builtin.com/machine-learning/fastest-sorting-algorithm
+// time complexity: O(n^2)/O(n) worst/best
+// space complexity: O(1)
+
+// In this sorting algorithm, we check to see if the order is correct for each element until we reach the current element. Since the first element is in order, we start from the second element and check if the order is maintained. If not, then we swap them. So, on any given element, we check if the current element is greater than the previous element. If it’s not, we keep swapping elements until our current element is greater than the previous element.
+
+namespace algorithms
+{
+
+class InsertionSort
+{
+public:
+    static void Sort(std::vector<int>& arr)
+    {
+        for (int i = 1; i < arr.size(); i++)
+        {
+            for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--)
+            {
+                std::swap(arr[j - 1], arr[j]);
+            }
+        }
+    }
+};
+
+}
+
+
