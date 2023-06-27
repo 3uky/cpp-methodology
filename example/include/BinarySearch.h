e4c86388ac7f3923133bcb9eda0dc76ea03709f1
hynek <hynek.bucek@gmail.com>
2023-06-27 11:27:48 +0200

    binary search added recursive version + unit test

--- example/include/BinarySearch.h
+++ example/include/BinarySearch.h
@@ -1,10 +1,9 @@
-/*
-- logarithmic complexity
-- could be used on sorted data
-- very fast
-- https://www.geeksforgeeks.org/binary-search/
-- start in middle compare if value is less or greater then middle / root if less continue on left if greater continue on right half, continue with same alg.
+// https://www.geeksforgeeks.org/binary-search/
+// could be used on sorted data
+// very fast
+// start in middle compare if value is less or greater then middle / root if less continue on left if greater continue on right half, continue with same alg.
 
+/*
 binarySearch(arr, x, low, high)
 	repeat till low = high
 		mid = (low + high)/2
@@ -16,22 +15,51 @@ binarySearch(arr, x, low, high)
 				high = mid - 1
 */
 
+// time complexity: O(log N) / recursive O(1)/O(log N)
+// space complexity: O(1) / recursive O(1)
+ 
+
 #pragma once
 
-int BinarySearch(int arr[], int l, int r, int x)
+#include <vector>
+
+class BinarySearch
 {
-	while (l <= r) {
-		int m = l + (r - l) / 2;
-		// Check if x is present at mid
-		if (arr[m] == x)
-			return m;
-		// If x greater, ignore left half
-		if (arr[m] < x)
-			l = m + 1;
-		// If x is smaller, ignore right half
-		else
-			r = m - 1;
+public:
+	static int Search(std::vector<int> arr, int l, int r, int x)
+	{
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+			// Check if x is present at mid
+			if (arr[m] == x)
+				return m;
+			// If x greater, ignore left half
+			if (arr[m] < x)
+				l = m + 1;
+			// If x is smaller, ignore right half
+			else
+				r = m - 1;
+		}
+		// if we reach here, then element was not present
+		return -1;
 	}
-	// if we reach here, then element was not present
-	return -1;
-}
+
+	static int RecursiveSearch(std::vector<int> arr, int l, int r, int x)
+	{
+		if (r >= l) 
+		{
+			int mid = l + (r - l) / 2;
+
+			if (arr[mid] == x)
+				return mid;
+
+			if (arr[mid] > x)
+				return RecursiveSearch(arr, l, mid - 1, x);
+
+			return RecursiveSearch(arr, mid + 1, r, x);
+		}
+
+		// element is not present in array
+		return -1;
+	}
+};
--- example_test/BinarySearchUT.cpp
+++ example_test/BinarySearchUT.cpp
@@ -1,23 +1,39 @@
 #include "pch.h"
 #include "BinarySearch.h"
 
+#include <vector>
+
 using namespace std;
 
 namespace algorithms
 {
 
-TEST(AlgorightmsTest, ShouldFindElementWithBinarySearch)
+TEST(BinarySearchTest, ShouldFindElement)
 {
 	// GIVEN
-	int arr[] = { 2, 3, 4, 10, 40 };
+	vector<int> arr = { 2, 3, 4, 10, 40 };
 	int x = 10;
-	int n = sizeof(arr) / sizeof(arr[0]);
+
 
 	// WHEN
-	int result = BinarySearch(arr, 0, n - 1, x);
+	int result = BinarySearch::Search(arr, 0, int(arr.size()) - 1, x);
 
 	// THEN
 	EXPECT_EQ(result, 3);
 }
+
+TEST(BinarySearchTest, ShouldFindElementWithRecursiveSearch)
+{
+	// GIVEN
+	vector<int> arr = { 2, 3, 4, 10, 40 };
+	int x = 10;
+
+	// WHEN
+	int result = BinarySearch::RecursiveSearch(arr, 0, int(arr.size()) - 1, x);
+
+	// THEN
+	EXPECT_EQ(result, 3);
+}
+
 
 }
