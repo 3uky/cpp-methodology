e2b7f1990045d640c184f0a0bf6c68f163bf18b6
hynek <hynek.bucek@gmail.com>
2023-06-28 17:37:20 +0200

    add smart pointer queue implementation

--- example/include/Queue.h
+++ example/include/Queue.h
@@ -5,15 +5,6 @@
 namespace containers
 {
 
-class QueueElement
-{
-public:
-	QueueElement(int value) : data(value) {}
-
-	int data;
-	std::unique_ptr<QueueElement> next = nullptr;
-};
-
 class IQueue
 {
 public:
@@ -24,10 +15,11 @@ public:
 	virtual int Size() const = 0; //Size : Returns the total number of elements present in the queue.
 };
 
-class Queue : public IQueue
+class QueueFromVector : public IQueue
 {
 public:
-	Queue() {};
+	QueueFromVector() {}
+
 	void Enqueue(int value) override
 	{
 		m_queue.push_back(value);
@@ -57,4 +49,76 @@ private:
 	std::list<int> m_queue;
 };
 
+
+class QueueElement
+{
+public:
+	QueueElement(int value) : data(value) {}
+
+	int data;
+	std::shared_ptr<QueueElement> next = nullptr;
+};
+
+class Queue : public IQueue
+{
+public:
+	void Enqueue(int value) override
+	{
+		const auto new_element = std::make_shared<QueueElement>(value);
+		if (m_front == nullptr)
+		{
+			m_front = new_element;
+			m_back = m_front;
+		}
+		else
+		{
+			m_back->next = new_element;
+			m_back = new_element;
+		}
+	}
+
+	void Dequeue() override
+	{
+		if (m_front == nullptr)
+			return;
+
+		m_front = m_front->next;
+
+		if (m_front == nullptr)
+			m_back = nullptr;		
+	}
+
+	int Peek() const override
+	{
+		if (m_front == nullptr)
+			throw;
+
+		return m_front->data;
+		
+	}
+
+	bool IsEmpty() const override
+	{
+		return m_front == nullptr;
+	}
+
+	int Size() const override
+	{
+		int size = 0;
+		std::shared_ptr<QueueElement> tmp = m_front;
+
+		while (tmp != nullptr)
+		{
+			tmp = tmp->next;
+			size++;
+		}
+
+		return size;
+	}
+
+private:
+	std::shared_ptr<QueueElement> m_front = nullptr;
+	std::shared_ptr<QueueElement> m_back = nullptr;
+};
+	
 }
