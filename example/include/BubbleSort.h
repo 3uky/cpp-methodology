7991d2100db48615dfadf507ff1117ffab51a028
3uky <hynek.bucek>
2023-08-09 13:55:42 +0200

    bubble sort - refactoring, doc

--- example/include/BubbleSort.h
+++ example/include/BubbleSort.h
@@ -1,7 +1,12 @@
 #pragma once
 
+// https://builtin.com/machine-learning/fastest-sorting-algorithm
+// time complexity: O(n^2) worst/best
+// space complexity: O(1)
+
 #include <algorithm>
 #include <vector>
+
 namespace algorithms
 {
 
@@ -14,11 +19,11 @@ public:
 
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < n - 1; j++)
+            for (int j = 1; j < n; j++)
             {
-                if (arr[j] > arr[j + 1])
+                if (arr[j - 1] > arr[j])
                 {
-                    std::swap(arr[j], arr[j + 1]);
+                    std::swap(arr[j - 1], arr[j]);
                 }
             }
         }
