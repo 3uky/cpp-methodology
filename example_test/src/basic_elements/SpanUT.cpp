9b4441a72ed8d634f4bcdb7c414f8e4c2739acf7
3uky <hynek.bucek@gmail.com>
2023-10-01 14:20:19 +0200

    std::span usage UT

---
+++ example_test/src/basic_elements/SpanUT.cpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "pch.h"
+
+#include <span>
+#include <array>
+
+namespace basic_elements
+{
+    int span_func_sum(std::span<const int> data) // since C++20
+    {
+        int result = 0;
+        for (const int e : data)
+            result += e;
+        return result;
+    }
+
+    TEST(SpanTest, ShouldReturnConainerValuesSummary)
+    {
+        // GIVEN
+        std::array<int, 4> container{1, 2, 3, 4};
+
+        // std::span (C++20) is a safer alternative to separated pointer/size.
+        int result = span_func_sum({ container.data(), container.size() });
+
+        // THEN
+        ASSERT_EQ(result, 10);
+    }
+}
