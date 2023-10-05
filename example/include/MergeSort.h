b39f60dbb801444039338d532aa65f75382547b5
3uky <hynek.bucek@gmail.com>
2023-10-05 14:27:20 +0200

    merge sort separate c-like array and vector version of merge alg.

--- example/include/MergeSort.h
+++ example/include/MergeSort.h
@@ -13,12 +13,12 @@ namespace algorithms
     public:
         static void Sort(std::vector<int>& arr)
         {
-            Sort(arr, 0, static_cast<int>(arr.size()) - 1);
+            Sort(arr.data(), 0, static_cast<int>(arr.size()) - 1);
         }
 
     private:
         // The below function will recursively break the array into two sub - arrays until each sub - array has only one element.
-        static void Sort(std::vector<int>& arr, int l, int r)
+        static void Sort(int* arr, int l, int r)
         {
             if (l < r) {
                 //int m = l + (r - l) / 2;
@@ -31,12 +31,12 @@ namespace algorithms
 
         // After that, we store these sub-arrays in two new arrays. Now, we can start merging them based on their order,
         // and store them into our input array. After all these subarrays are merged, our input array will be sorted.
-        static void Merge(std::vector<int>& arr, int l, int m, int r)
+        static void Merge(int* arr, int l, int m, int r)
     	{
             const int n1 = m - l + 1;
             const int n2 = r - m;
-            std::vector<int> L(n1);
-            std::vector<int> R(n2);
+            int* L = new int(n1);
+            int* R = new int(n2);
 
         	for (int i = 0; i < n1; i++)
             {
@@ -70,6 +70,38 @@ namespace algorithms
             }
         }
     };
+
+
+    static void MergeVectorVersion(std::vector<int>& arr, int l, int m, int r)
+    {
+        std::vector<int> L(arr.cbegin() + l, arr.cbegin() + m + 1); // initialization <first, last) last is not included
+        std::vector<int> R(arr.cbegin() + m + 1, arr.cbegin() + r + 1);
+        const auto n1 = L.size();
+        const auto n2 = R.size();
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k++] = L[i++];
+            }
+            else
+            {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1)
+        {
+            arr[k++] = L[i++];
+        }
+        while (j < n2)
+        {
+            arr[k++] = R[j++];
+        }
+    }
+
 
 }
 
