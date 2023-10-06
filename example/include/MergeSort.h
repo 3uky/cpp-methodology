d9687d4bbed2f32a00c8c715dc5c0306bc10c83e
3uky <hynek.bucek@gmail.com>
2023-10-06 15:26:04 +0200

    merge sort - new simplified version

--- example/include/MergeSort.h
+++ example/include/MergeSort.h
@@ -13,19 +13,19 @@ namespace algorithms
     public:
         static void Sort(std::vector<int>& arr)
         {
-            Sort(arr.data(), 0, static_cast<int>(arr.size()) - 1);
+            Sort(arr, 0, static_cast<int>(arr.size()) - 1);
         }
 
     private:
         // The below function will recursively break the array into two sub - arrays until each sub - array has only one element.
-        static void Sort(int* arr, int l, int r)
+        static void Sort(std::vector<int>& arr, int l, int r)
         {
             if (l < r) {
                 int m = l + (r - l) / 2;
                 //int m = (l + r) / 2; // could overflow
                 Sort(arr, l, m);
                 Sort(arr, m + 1, r);
-                Merge(arr, l, m, r);
+                Merge(arr.data(), l, m, r);
             }
         }
 
@@ -72,39 +72,56 @@ namespace algorithms
             delete [] L;
             delete [] R;
         }
-    };
 
-
-    static void MergeVectorVersion(std::vector<int>& arr, int l, int m, int r)
-    {
-        std::vector<int> L(arr.cbegin() + l, arr.cbegin() + m + 1); // initialization <first, last) last is not included
-        std::vector<int> R(arr.cbegin() + m + 1, arr.cbegin() + r + 1);
-        const auto n1 = L.size();
-        const auto n2 = R.size();
-
-        int i = 0, j = 0, k = l;
-        while (i < n1 && j < n2)
+        static void MergeVectorVersion(std::vector<int>& arr, int l, int m, int r)
         {
-            if (L[i] <= R[j])
+            std::vector<int> L(arr.cbegin() + l, arr.cbegin() + m + 1); // initialization <first, last) last is not included
+            std::vector<int> R(arr.cbegin() + m + 1, arr.cbegin() + r + 1);
+            const auto n1 = L.size();
+            const auto n2 = R.size();
+
+            int i = 0, j = 0, k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k++] = L[i++];
+                }
+                else
+                {
+                    arr[k++] = R[j++];
+                }
+            }
+
+            while (i < n1)
             {
                 arr[k++] = L[i++];
             }
-            else
+            while (j < n2)
             {
                 arr[k++] = R[j++];
             }
         }
 
-        while (i < n1)
+        static void MergeFinalVersion(std::vector<int>& arr, int l, int m, int r)
         {
-            arr[k++] = L[i++];
-        }
-        while (j < n2)
-        {
-            arr[k++] = R[j++];
-        }
-    }
+            std::vector<int> L(arr.cbegin() + l, arr.cbegin() + m + 1); // initialization <first, last) last is not included
+            std::vector<int> R(arr.cbegin() + m + 1, arr.cbegin() + r + 1);
 
+            int i = l, j = 0, k = 0;
+
+            while (i < r)
+            {
+                if (L[j] < R[k])
+                    arr[i++] = L[j++];
+                else
+                    arr[i++] = R[k++];
+            }
+
+            while (j < L.size())
+                arr[i++] = L[j++];
+            while (k < R.size())
+                arr[i++] = R[k++];
+        }
+    };
 }
-
-
