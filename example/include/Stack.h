abd01bf7cf13a134e25b59066ee156594a552839
3uky <hynek.bucek@gmail.com>
2023-11-28 13:08:54 +0100

    interfaces moved to shared folder

--- example/include/Stack.h
+++ example/include/Stack.h
@@ -3,17 +3,10 @@
 #include <deque>
 #include <vector>
 
+#include "IStack.h"
+
 namespace containers
 {
-	class IStack
-	{
-	public:
-		virtual void Push(int value) = 0;
-		virtual void Pop() = 0;
-		virtual int Top() const = 0;
-		virtual bool Empty() const = 0;
-	};
-
 	class StackFromDeque : public IStack
 	{
 	public:
---
+++ example/shared/IStack.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace containers
+{
+	class IStack
+	{
+	public:
+		virtual void Push(int value) = 0;
+		virtual void Pop() = 0;
+		virtual int Top() const = 0;
+		virtual bool Empty() const = 0;
+	};
+}
