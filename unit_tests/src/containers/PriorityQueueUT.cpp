9c53eec85930f290163d9102c0cb287478a5575c
3uky <hynek.bucek@gmail.com>
2023-12-01 10:58:53 +0100

    added method for access underlying container for priority queue, method for conversion to vector doesn't destruct container content

--- unit_tests/src/containers/PriorityQueueUT.cpp
+++ unit_tests/src/containers/PriorityQueueUT.cpp
@@ -7,8 +7,7 @@ using ::testing::UnorderedElementsAre;
 
 namespace exercises
 {
-
-    std::vector<int> PriorityQueueToVector(std::priority_queue<int, std::vector<int>, std::greater<int>>& queue)
+    std::vector<int> PriorityQueueToVector(std::priority_queue<int, std::vector<int>, std::greater<int>> queue)
     {
         std::vector<int> result;
         while (!queue.empty())
@@ -18,23 +17,35 @@ namespace exercises
         }
         return result;
     }
+    
+    // https://stackoverflow.com/questions/75242757/should-the-underlying-container-of-a-priority-queue-be-considered-a-complete-bin
+    template <typename T, typename Container, typename Comp>
+    const Container& GetUnderlyingContainer(const std::priority_queue<T, Container, Comp>& queue)
+    {
+        struct access_t : std::priority_queue<T, Container, Comp>
+        {
+            auto member_ptr() 
+            { 
+                return &access_t::c;
+            }
+        } access;
+        
+        auto ptr = access.member_ptr();
+        return queue.*ptr;
+    }
 
     TEST(PriorityQueueTest, ShouldReturnGreatestValues)
     {
         // GIVEN
-        std::vector input_data{1, 2, 3, 13, 16, 4, 5, 6, 7, 8, 9, 10, 11, 22, 33};
+        const auto input_data = {1, 2, 3, 13, 16, 4, 5, 6, 7, 8, 9, 10, 11, 22, 33};
         constexpr int max_elements = 5;
-        std::priority_queue<int, std::vector<int>, std::greater<int>> myQueue;
+        std::priority_queue<int, std::vector<int>, std::greater<int>> myQueue(input_data.begin(), input_data.end());
 
         // WHEN
-        for(auto value : input_data)
-        {
-            myQueue.emplace(value);
-            if (myQueue.size() > max_elements)
-                myQueue.pop();
-        }
+        while(myQueue.size() > max_elements)
+            myQueue.pop();
 
         // THEN
-        ASSERT_THAT(PriorityQueueToVector(myQueue), UnorderedElementsAre(11, 13, 16, 22, 33));
+        ASSERT_THAT(GetUnderlyingContainer(myQueue), UnorderedElementsAre(11, 13, 16, 22, 33));
     }
 }
