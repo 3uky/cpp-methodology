71c264c73d3d28cdbad3d90de3942eb58d119c30
3uky <hynek.bucek@gmail.com>
2024-09-06 14:04:25 +0200

    unit test - added test demonstrating database mocking

---
+++ unit_tests/src/unit_test/databaseSimulationUT.cpp
@@ -0,0 +1,55 @@
+#include "gmock/gmock.h"
+
+#include <memory>
+
+using ::testing::Return;
+
+namespace unit_test
+{
+    class Database
+    {
+    public:
+        virtual int GetData() = 0;
+    };
+
+    class MockDatabase : public Database
+    {
+    public:
+        MOCK_METHOD(int, GetData, (), (override));
+    };
+
+    class User
+    {
+    public:
+        User() {};
+
+        void SetDatabase(std::shared_ptr<Database> database)
+        {
+            m_database = database;
+        }
+
+        int ExtractData()
+        {
+            return m_database->GetData();
+        }
+
+        std::shared_ptr<Database> m_database;
+    };
+
+TEST(DatabaseSimulationTest, ShouldSimulateGettingDataFromDatabase )
+{
+    // GIVEN
+    std::shared_ptr<MockDatabase> database = std::make_shared<MockDatabase>(); 
+    User user;
+    user.SetDatabase(database);
+    int expected_value = 5;
+
+    EXPECT_CALL(*database, GetData()).WillOnce(Return(5)); // simulate behavior of database
+
+    // WHEN
+    int result = user.ExtractData();
+
+    // THEN
+    EXPECT_EQ(result, expected_value);
+}
+}
