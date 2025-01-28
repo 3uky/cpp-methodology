e4c411459b661bb94ed9ed6a36a304e715b03b42
3uky <hynek.bucek@gmail.com>
2025-01-28 15:21:31 +0100

    condition variable unit tests update

--- tst/basic/parallel/ConditionVariableUT.cpp
+++ tst/basic/parallel/ConditionVariableUT.cpp
@@ -9,7 +9,7 @@ using namespace std;
 namespace parallel_programming
 {
 
-TEST(ParalellProgrammingTest, ShouldThreadsWaitOnConditionVariableBeforeIncremenCounter)
+TEST(ConditionVariableTest, ShouldThreadsWaitOnConditionVariableBeforeIncremenCounter)
 {
     // GIVEN
     constexpr int numberOfThreads = 10;
@@ -30,6 +30,35 @@ TEST(ParalellProgrammingTest, ShouldThreadsWaitOnConditionVariableBeforeIncremen
 
     // THEN
     EXPECT_EQ(myConditionVariable.GetCounter(), numberOfThreads);
+}
+
+
+TEST(ConditionVariableTest, WaitAndNotify) {
+    std::mutex mtx;
+    std::condition_variable cv;
+    bool ready = false;
+
+    // thread that waits for the signal
+    std::thread waiter([&]() {
+        std::unique_lock<std::mutex> lock(mtx);
+        while (!ready) {
+            cv.wait(lock);  // wait until notification from notifier (unique lock is released during wait and lock again after notification)
+        }
+        ASSERT_TRUE(ready);
+    });
+
+    // thread that signals the other thread
+    std::thread notifier([&]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // simulate work
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            ready = true;
+        }
+        cv.notify_one();  // notify waiting thread
+    });
+
+    waiter.join();
+    notifier.join();
 }
 
 }
