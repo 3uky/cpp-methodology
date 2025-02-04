13280f60aa53abfd5dd2a564111dadfeaecc472a
3uky <hynek.bucek@gmail.com>
2025-02-04 23:34:38 +0100

    parallel programming - refactoring simple mutex and lock_guard unit tests

---
+++ tst/parallel_programming/lock_guard.cpp
@@ -0,0 +1,90 @@
+#include "gmock/gmock.h"
+
+#include <chrono>
+
+using namespace std;
+
+using testing::ContainsRegex;
+using testing::MatchesRegex;
+
+namespace parallel_programming
+{
+
+TEST(MutexTest, ProtectCriticalSectionWithMutex)
+{
+    // GIVEN
+    std::mutex mtx;
+
+    // critical section (exclusive access to std::cout signaled by locking mtx):
+	auto PrintBlockWithMutex = [&](int n, char c) {
+        mtx.lock();
+        for (int i = 0; i < n; ++i)
+	        cout << c;
+        mtx.unlock();
+    };
+
+    thread th1(PrintBlockWithMutex, 5000, 'A');
+    thread th2(PrintBlockWithMutex, 5000, 'B');
+
+    // WHEN
+    testing::internal::CaptureStdout();
+    th1.join();
+    th2.join();
+
+    // THEN
+    ASSERT_THAT(::testing::internal::GetCapturedStdout(), MatchesRegex("A+B+"));
+}
+
+TEST(MutexTest, ProtectCriticalSectionWithLockGuard)
+{
+    // GIVEN
+    std::mutex mtx;
+
+    // critical section (exclusive access to std::cout signaled by locking mtx):
+    auto PrintBlockWithGuard = [&](int n, char c) {
+        lock_guard<std::mutex> lck(mtx);
+
+        for (int i = 0; i < n; ++i) {
+            cout << c;
+        }
+    };
+    
+    // WHEN
+    thread th1(PrintBlockWithGuard, 5000, 'A');
+    thread th2(PrintBlockWithGuard, 5000, 'B');
+    
+    testing::internal::CaptureStdout();
+    th1.join();
+    th2.join();
+
+    // THEN
+    ASSERT_THAT(::testing::internal::GetCapturedStdout(), MatchesRegex("A+B+"));
+}
+
+TEST(MutexTest, ProtectCriticalSectionWithLockGuardWithLotOfThreads) 
+{
+    // GIVEN
+    std::mutex mtx;
+    std::vector<std::thread> threads;
+    int shared_data = 0;
+    int no_threads = 1000;
+
+    // without protection of critical section race condition could occure
+    // race condition scenario: thread A read value of shared_data before increment and it's interupted with thread B
+    auto increment = [&]() {
+        std::lock_guard<std::mutex> lock(mtx);
+        shared_data++;
+    };
+
+    // WHEN
+    for (int i = 0; i < no_threads; ++i) 
+        threads.push_back(std::thread(increment));
+
+    for (auto& t : threads)
+        t.join();
+
+    // THEN
+    ASSERT_EQ(shared_data, no_threads);
+}
+
+}
---
+++ tst/parallel_programming/thread.cpp
@@ -0,0 +1,36 @@
+#include "gmock/gmock.h"
+
+#include <chrono>
+
+namespace parallel_programming
+{
+
+TEST(MutexTest, executeMultipleThreads)
+{
+    GTEST_SKIP();
+
+    // GIVEN
+    int pause_duration_s = 1;
+    auto PauseThread = [&](int n) {
+        std::this_thread::sleep_for(std::chrono::seconds(n));
+        std::cout << "pause of " << n << " seconds ended" << std::endl;
+    };
+
+    std::thread t1(PauseThread, pause_duration_s);
+    std::thread t2(PauseThread, pause_duration_s);
+    std::thread t3(PauseThread, pause_duration_s);
+
+    // WHEN
+    auto const start = std::chrono::steady_clock::now();
+    t1.join();
+    t2.join();
+    t3.join();
+    auto const end = std::chrono::steady_clock::now();
+    const std::chrono::duration<double> elapsed_seconds = end - start;
+
+    // THEN
+    EXPECT_GE(elapsed_seconds.count(), pause_duration_s);
+}
+
+
+}
