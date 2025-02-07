a5610b27cdee8ddb1bd26edf85eb6cf5f9660583
3uky <hynek.bucek@gmail.com>
2025-02-07 13:47:21 +0100

    synchronization problems - race condition unit test

--- tst/synchronization_problems/race_condition.cpp
+++ tst/synchronization_problems/race_condition.cpp
@@ -3,21 +3,24 @@
 #include <thread>
 #include <atomic>
 
-namespace parallel_programming 
+namespace synchronization_problems 
 {
-
-std::atomic<int> counter(0);
-
-void increment() 
-{
-    for (int i = 0; i < 100000; ++i)
-        counter++;
-}
 
 // Without proper synchronization an incorrect result of counter is expected.
 // The expected result (200000) is unlikely due to the race condition.
 TEST(RaceConditionTest, TestRaceCondition) 
 {
+    // std::atomic<int> counter(0); // one possible solution of this race condition is usage of thread safe counter
+    int counter(0);
+
+    auto increment = [&]() 
+    {
+        for (int i = 0; i < 100000; ++i)
+        {
+            counter++;
+        }
+    };
+
     std::thread t1(increment);
     std::thread t2(increment);
     
