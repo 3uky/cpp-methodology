5f0a590a985ea1969b59d6f638b5d4fb31590d05
3uky <hynek.bucek@gmail.com>
2025-02-09 14:38:05 +0100

    synchronization problems - solution of starvation with condition variable unit test

--- tst/synchronization_problems/starvation.cpp
+++ tst/synchronization_problems/starvation.cpp
@@ -50,4 +50,62 @@ TEST(SyncrhonizationProblemsTest, TestStarvation)
     ASSERT_LT(counterLowThread, counterHighThread);
 }
 
+TEST(SyncrhonizationProblemsTest, TestStarvationSolvedWithConditionVariable)
+{
+    GTEST_SKIP();
+
+    int no_runs = 1000;
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::atomic<int> counterHighThread(0);
+    std::atomic<int> counterLowThread(0);
+    std::atomic<int> counterAllThreads(0);
+    // a flag to control which thread should acquire the lock next (to ensure fairness)
+    // 0 means high-priority thread, 1 means low-priority thread
+    std::atomic<int> turn(0);
+
+    auto high_priority_thread= [&]() 
+    {
+        while (counterAllThreads < no_runs)
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            // unlock lock during wait (when lambda return true it stops waiting and lock lock)
+            cv.wait(lock, [&turn] { return turn == 0; });
+
+            ++counterAllThreads;
+            ++counterHighThread;
+
+            turn = 1;
+            cv.notify_all();
+        }
+    };
+
+    // low-priority thread: sleeps to simulate being less active
+    auto low_priority_thread = [&]() {
+        while (counterAllThreads < no_runs) 
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [&turn] { return turn == 1; });
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+            ++counterAllThreads;
+            ++counterLowThread;
+
+            turn = 0;
+            cv.notify_all();
+        }
+    };
+
+    std::thread t1(high_priority_thread);
+    std::thread t2(low_priority_thread);
+
+    t1.join();
+    t2.join();
+
+    // the counters should be close to each other since both threads get a fair chance
+    ASSERT_NEAR(counterHighThread, no_runs/2, 1);
+    ASSERT_NEAR(counterLowThread, no_runs/2, 1);
+}
+
 }
